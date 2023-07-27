abc0bc0369e1c5ec26e2fba7633f84aaad28bd50
bruno-rodrigues-d <brunorodrigues@gec.inatel.br>
2023-07-27 07:17:57 -0300

    feat: populating profile information

--- src/pages/Blog/components/Profile/index.tsx
+++ src/pages/Blog/components/Profile/index.tsx
@@ -1,36 +1,39 @@
 import { ArrowSquareOut, GithubLogo, MapPin, Users } from "phosphor-react";
 import { ProfileContainer, ProfileInfo, ProfileTitle, ProfileLink, ProfileFooter, ProfileIcons } from "./styles";
 
-export function Profile() {
+interface ProfileProps {
+  information: any
+}
+
+export function Profile({ information }: ProfileProps) {
   return(
     <>
       <ProfileContainer>
-        <img src="https://github.com/bruno-rodrigues-d.png" alt="" />
+        <img src={information.avatar_url} alt="" />
 
         <ProfileInfo>
           <ProfileTitle>
-            <span>Bruno Rodrigues</span>
-            <ProfileLink href='/'>
+            <span>{information.name}</span>
+            <ProfileLink href={information.html_url} target='_blank'>
               <span>GITHUB</span>
               <ArrowSquareOut size={16} color="#3294F8" />
             </ProfileLink>
           </ProfileTitle>
           <p>
-            Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu viverra
-            massa quam dignissim aenean malesuada suscipit. Nunc, volutpat pulvinar vel mass.
+            {information.bio}
           </p>
           <ProfileFooter>
             <ProfileIcons>
               <GithubLogo size={18} color="#3A536B" />
-              <p>bruno-rodrigues-d</p>
+              <p>{information.login}</p>
             </ProfileIcons>
             <ProfileIcons>
               <MapPin size={18} color="#3A536B" />
-              <p>São Paulo - SP</p>
+              <p>{information.location}</p>
             </ProfileIcons>
             <ProfileIcons>
               <Users size={18} color="#3A536B" />
-              <p>32 seguidores</p>
+              <p>{information.followers} seguidores</p>
             </ProfileIcons>
           </ProfileFooter>
         </ProfileInfo>
--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,12 +1,30 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Post } from "./components/Post";
 import { Profile } from "./components/Profile";
 import { Search } from "./components/Search";
 import { ListPost } from "./styles";
 
 export function Blog() {
+  const [profileInformation, setProfileInformation] = useState([]);
+
+  async function handleGithubProfileInformation() {
+    try {
+      const response = await axios.get('https://api.github.com/users/bruno-rodrigues-d')
+
+      setProfileInformation(response.data);
+    } catch (err) {
+      alert('Falha ao encontrar o perfil no GitHub!');
+    }
+  }
+
+  useEffect(() => {
+    handleGithubProfileInformation();
+  }, []);
+
   return(
     <>
-      <Profile />
+      <Profile information={profileInformation} />
       <Search />
       <ListPost>
         <Post />
